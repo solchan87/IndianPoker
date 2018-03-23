c6a15ef12b553a765516603a5ff67cb17fe8b75c
solchan87 <perudot@gmail.com>
2018-03-23 20:34:45 +0900

    start

---
+++ IndianPoker/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  IndianPoker
+//
+//  Created by SolChan Ahn on 2018. 3. 23..
+//  Copyright © 2018년 SolChan Ahn. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+var databaseReference: DatabaseReference!
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        databaseReference = Database.database().reference()
+        
+        let testDB = databaseReference.child("test").child("1")
+        testDB.observe(DataEventType.value) { (snapshot) in
+            let value = snapshot.value as? String
+            
+            print(value)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
